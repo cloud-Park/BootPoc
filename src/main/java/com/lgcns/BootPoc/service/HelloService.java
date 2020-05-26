480a8f50303edfbdbd05172f187a08f8110be199
ms.chae <ms.chae@lgcns.com>
2020-05-26 16:29:02 +0900

    MariaDB 연동완료

--- src/main/java/com/lgcns/BootPoc/service/HelloService.java
+++ src/main/java/com/lgcns/BootPoc/service/HelloService.java
@@ -1,16 +1,27 @@
 package com.lgcns.BootPoc.service;
 
 import com.lgcns.BootPoc.repository.HelloRepository;
+import com.lgcns.BootPoc.repository.UserEntity;
+import com.lgcns.BootPoc.repository.UserRepository;
 import com.lgcns.BootPoc.web.dto.HelloResponseDto;
+import com.lgcns.BootPoc.web.dto.UserResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class HelloService {
     private final HelloRepository helloRepository;
+    private final UserRepository userRepository;
 
     public HelloResponseDto searchById(String id){
         return helloRepository.searchById(id);
+    }
+
+    public List<UserResponseDto> findAll(){
+        return userRepository.findAll().stream().map(UserResponseDto::new).collect(Collectors.toList());
     }
 }
