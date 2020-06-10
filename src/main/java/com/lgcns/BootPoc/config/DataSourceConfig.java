50154c94f7ceb2a788f05533241a96bbd5173a36
dohk0826 <dohk0826@gmail.com>
2020-06-10 16:38:34 +0900

    -

--- src/main/java/com/lgcns/BootPoc/config/DataSourceConfig.java
+++ src/main/java/com/lgcns/BootPoc/config/DataSourceConfig.java
@@ -5,17 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import redis.embedded.RedisServer;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.sql.DataSource;
 
 @Slf4j
