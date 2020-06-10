9489f4c26da58a146e8c2bc3b9d8da4686ce441a
김도형 <1073796@hanwha.com>
2020-06-10 11:28:16 +0900

    commondao

--- src/main/java/com/lgcns/BootPoc/template/hello/service/HelloService.java
+++ src/main/java/com/lgcns/BootPoc/template/hello/service/HelloService.java
@@ -5,6 +5,8 @@ import com.lgcns.BootPoc.template.hello.repository.UserRepository;
 import com.lgcns.BootPoc.template.hello.dto.HelloResponseDto;
 import com.lgcns.BootPoc.template.hello.dto.UserResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,11 +18,18 @@ public class HelloService {
     private final HelloRepository helloRepository;
     private final UserRepository userRepository;
 
+    @Autowired
+    private final SqlSessionTemplate sqlSessionTemplate;
+
     public HelloResponseDto searchById(String id){
         return helloRepository.searchById(id);
     }
 
     public List<UserResponseDto> findAll(){
-        return userRepository.findAll().stream().map(UserResponseDto::new).collect(Collectors.toList());
+        List<UserResponseDto> list = sqlSessionTemplate.selectList("com.lgcns.BootPoc.template.hello.repository.UserRepository.findAll");
+        System.out.println(list);
+        return list;
+
+        //return userRepository.findAll().stream().map(UserResponseDto::new).collect(Collectors.toList());
     }
 }
