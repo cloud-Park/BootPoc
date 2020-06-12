86821ae57b004114107e7eb0c9b63d71b9b8433c
김도형 <1073796@hanwha.com>
2020-06-12 09:05:53 +0900

    CommonDao 와꾸

---
+++ src/main/java/com/lgcns/BootPoc/framework/dataaccess/CommonDao.java
@@ -0,0 +1,87 @@
+package com.lgcns.BootPoc.framework.dataaccess;
+
+
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.List;
+
+public interface CommonDao {
+    <T> T select(String var1);
+
+    <T> T select(String var1, Object var2);
+
+    <T> T select(String var1, Object var2, String var3);
+
+    <E> List<E> selectList(String var1);
+
+    <E> List<E> selectList(String var1, Object var2);
+
+    <E> List<E> selectList(String var1, Object var2, String var3);
+
+    <E> List<E> selectList(String var1, Object var2, RowBounds var3);
+
+    <E> List<E> selectList(String var1, Object var2, String var3, RowBounds var4);
+
+    <T> void selectList(String var1, ResultHandler<T> var2);
+
+    <T> void selectList(String var1, Object var2, ResultHandler<T> var3);
+
+    <E> List<E> selectPagedList(String var1, Object var2);
+
+    <E> List<E> selectPagedList(String var1, Object var2, boolean var3);
+
+    <E> List<E> selectPagedList(String var1, Object var2, String var3);
+
+    <E> List<E> selectPagedList(String var1, Object var2, String var3, boolean var4);
+
+    <E> List<E> selectScrollPagedList(String var1, Object var2);
+
+    <E> List<E> selectScrollPagedList(String var1, Object var2, String var3);
+
+    <E> List<E> selectScrollPagedList(String var1, Object var2, boolean var3);
+
+    <E> List<E> selectScrollPagedList(String var1, Object var2, String var3, boolean var4);
+
+    int insert(String var1);
+
+    int insert(String var1, Object var2);
+
+    int insert(String var1, Object var2, String var3);
+
+    int update(String var1);
+
+    int update(String var1, Object var2);
+
+    int update(String var1, Object var2, String var3);
+
+    int delete(String var1);
+
+    int delete(String var1, Object var2);
+
+    int delete(String var1, Object var2, String var3);
+
+    int batchInsert(String var1, List<?> var2);
+
+    int batchInsert(String var1, List<?> var2, boolean var3);
+
+    int batchInsert(String var1, List<?> var2, String var3);
+
+    int batchInsert(String var1, List<?> var2, String var3, boolean var4);
+
+    int batchUpdate(String var1, List<?> var2);
+
+    int batchUpdate(String var1, List<?> var2, boolean var3);
+
+    int batchUpdate(String var1, List<?> var2, String var3);
+
+    int batchUpdate(String var1, List<?> var2, String var3, boolean var4);
+
+    int batchDelete(String var1, List<?> var2);
+
+    int batchDelete(String var1, List<?> var2, boolean var3);
+
+    int batchDelete(String var1, List<?> var2, String var3);
+
+    int batchDelete(String var1, List<?> var2, String var3, boolean var4);
+}
---
+++ src/main/java/com/lgcns/BootPoc/framework/dataaccess/mybatis/MybatisCommonDao.java
@@ -0,0 +1,210 @@
+package com.lgcns.BootPoc.framework.dataaccess.mybatis;
+
+import com.lgcns.BootPoc.framework.dataaccess.CommonDao;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.beans.factory.InitializingBean;
+
+import java.util.List;
+
+public class MybatisCommonDao implements CommonDao, InitializingBean {
+    @Override
+    public <T> T select(String var1) {
+        return null;
+    }
+
+    @Override
+    public <T> T select(String var1, Object var2) {
+        return null;
+    }
+
+    @Override
+    public <T> T select(String var1, Object var2, String var3) {
+        return null;
+    }
+
+    @Override
+    public <E> List<E> selectList(String var1) {
+        return null;
+    }
+
+    @Override
+    public <E> List<E> selectList(String var1, Object var2) {
+        return null;
+    }
+
+    @Override
+    public <E> List<E> selectList(String var1, Object var2, String var3) {
+        return null;
+    }
+
+    @Override
+    public <E> List<E> selectList(String var1, Object var2, RowBounds var3) {
+        return null;
+    }
+
+    @Override
+    public <E> List<E> selectList(String var1, Object var2, String var3, RowBounds var4) {
+        return null;
+    }
+
+    @Override
+    public <T> void selectList(String var1, ResultHandler<T> var2) {
+
+    }
+
+    @Override
+    public <T> void selectList(String var1, Object var2, ResultHandler<T> var3) {
+
+    }
+
+    @Override
+    public <E> List<E> selectPagedList(String var1, Object var2) {
+        return null;
+    }
+
+    @Override
+    public <E> List<E> selectPagedList(String var1, Object var2, boolean var3) {
+        return null;
+    }
+
+    @Override
+    public <E> List<E> selectPagedList(String var1, Object var2, String var3) {
+        return null;
+    }
+
+    @Override
+    public <E> List<E> selectPagedList(String var1, Object var2, String var3, boolean var4) {
+        return null;
+    }
+
+    @Override
+    public <E> List<E> selectScrollPagedList(String var1, Object var2) {
+        return null;
+    }
+
+    @Override
+    public <E> List<E> selectScrollPagedList(String var1, Object var2, String var3) {
+        return null;
+    }
+
+    @Override
+    public <E> List<E> selectScrollPagedList(String var1, Object var2, boolean var3) {
+        return null;
+    }
+
+    @Override
+    public <E> List<E> selectScrollPagedList(String var1, Object var2, String var3, boolean var4) {
+        return null;
+    }
+
+    @Override
+    public int insert(String var1) {
+        return 0;
+    }
+
+    @Override
+    public int insert(String var1, Object var2) {
+        return 0;
+    }
+
+    @Override
+    public int insert(String var1, Object var2, String var3) {
+        return 0;
+    }
+
+    @Override
+    public int update(String var1) {
+        return 0;
+    }
+
+    @Override
+    public int update(String var1, Object var2) {
+        return 0;
+    }
+
+    @Override
+    public int update(String var1, Object var2, String var3) {
+        return 0;
+    }
+
+    @Override
+    public int delete(String var1) {
+        return 0;
+    }
+
+    @Override
+    public int delete(String var1, Object var2) {
+        return 0;
+    }
+
+    @Override
+    public int delete(String var1, Object var2, String var3) {
+        return 0;
+    }
+
+    @Override
+    public int batchInsert(String var1, List<?> var2) {
+        return 0;
+    }
+
+    @Override
+    public int batchInsert(String var1, List<?> var2, boolean var3) {
+        return 0;
+    }
+
+    @Override
+    public int batchInsert(String var1, List<?> var2, String var3) {
+        return 0;
+    }
+
+    @Override
+    public int batchInsert(String var1, List<?> var2, String var3, boolean var4) {
+        return 0;
+    }
+
+    @Override
+    public int batchUpdate(String var1, List<?> var2) {
+        return 0;
+    }
+
+    @Override
+    public int batchUpdate(String var1, List<?> var2, boolean var3) {
+        return 0;
+    }
+
+    @Override
+    public int batchUpdate(String var1, List<?> var2, String var3) {
+        return 0;
+    }
+
+    @Override
+    public int batchUpdate(String var1, List<?> var2, String var3, boolean var4) {
+        return 0;
+    }
+
+    @Override
+    public int batchDelete(String var1, List<?> var2) {
+        return 0;
+    }
+
+    @Override
+    public int batchDelete(String var1, List<?> var2, boolean var3) {
+        return 0;
+    }
+
+    @Override
+    public int batchDelete(String var1, List<?> var2, String var3) {
+        return 0;
+    }
+
+    @Override
+    public int batchDelete(String var1, List<?> var2, String var3, boolean var4) {
+        return 0;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+}
